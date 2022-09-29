2c2c2e9e7fa9c273e0ba361b8b5f87e4012fed0c
lan-e <neladuranec@gmail.com>
2022-09-29 14:58:12 +0200

    Dodani linkovi za Facebook Instagram dodan custom favicon.ico

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({children}) => {
   return (
     <div className='layout'>
       <Head>
-        <title>JS Mastery Store</title>
+        <title>SOPG Bakale</title>
       </Head>
       <header>
         <Navbar/>
