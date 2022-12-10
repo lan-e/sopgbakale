adab5b25754946512ccd5f6966bca7b816fbfdbc
lan-e <neladuranec@gmail.com>
2022-12-10 13:26:29 +0100

    changed instagram link in footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <div className='footer-container'>
       <p>2022 SOPG BAKALE<br/>All rights reserved</p>
       <p className='icons'>
-        <a href="https://www.instagram.com/opg.bakale/" target="_blank"><AiFillInstagram/></a>
+        <a href="https://www.instagram.com/sopg.bakale/" target="_blank"><AiFillInstagram/></a>
         <a href="https://www.facebook.com/profile.php?id=100064179074932" target="_blank"><AiFillFacebook/></a>
         
       </p>
